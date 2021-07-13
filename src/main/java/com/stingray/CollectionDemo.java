a56b26d47beb404eb81998caee3732913e6be860
sande <sande@DESKTOP-TAEAA0R>
2021-07-13 22:51:20 +0530

    Use HashCodeBuilder and EqualBuilder to override the hashcode and equals method

--- src/main/java/com/stingray/CollectionDemo.java
+++ src/main/java/com/stingray/CollectionDemo.java
@@ -1,6 +1,5 @@
 package com.stingray;
 
-import java.security.cert.PKIXRevocationChecker.Option;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
@@ -8,6 +7,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
 
 import com.stingray.dataSoruce.Employee;
 import com.stingray.dataSoruce.EmployeeDao;
@@ -19,6 +21,14 @@ import lombok.Setter;
 public class CollectionDemo {
 
 	public static void main(String[] args) throws Exception {
+		
+		int k = 1011 & 0111;
+		System.out.println(k);
+		
+	}
+	
+	static void go() throws ParseException {
+
 		List<Employee> allEmployee = EmployeeDao.getAllEmployee();
 		// Map<Integer, String> employeeMap =
 		// allEmployee.stream().collect(Collectors.toMap(Employee::getEmpId,
@@ -36,19 +46,22 @@ public class CollectionDemo {
 
 		empMap.forEach((k, v) -> {
 			Optional<String> optionalEmpName = Optional.ofNullable(v.getEmpName());
-			
-				try {
-					System.out.println( optionalEmpName.orElseThrow(()->new Exception("test")));
-				} catch (Exception e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			
-			/*if(optionalEmpName.isPresent())
-				System.out.println( optionalEmpName.orElse("test"));*/
-			
+
+			try {
+				System.out.println(optionalEmpName.orElseThrow(() -> new Exception("test")));
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			/*
+			 * if(optionalEmpName.isPresent()) System.out.println(
+			 * optionalEmpName.orElse("test"));
+			 */
+
 		});
 
+	
 	}
 
 }
@@ -58,15 +71,31 @@ public class CollectionDemo {
 @AllArgsConstructor
 class EmployeeKey {
 	Integer id;
-
-	@Override
-	public int hashCode() {
-		return id;
-	}
+	/*
+	 * @Override public int hashCode() { final int prime = 31; int result = 1;
+	 * result = prime * result + ((id == null) ? 0 : id.hashCode()); return result;
+	 * }
+	 * 
+	 * @Override public boolean equals(Object obj) { if (this == obj) return true;
+	 * if (obj == null) return false; if (getClass() != obj.getClass()) return
+	 * false; EmployeeKey other = (EmployeeKey) obj; if (id == null) { if (other.id
+	 * != null) return false; } else if (!id.equals(other.id)) return false; return
+	 * true; }
+	 * 
+	 */
 
 	@Override
 	public boolean equals(Object obj) {
-		return false;
+		EmployeeKey key = (EmployeeKey) obj;
+		EqualsBuilder builder = new EqualsBuilder();
+		builder.append(this.getId(), key.getId());
+		return builder.isEquals();
 	}
 
+	@Override
+	public int hashCode() {
+		HashCodeBuilder builder = new HashCodeBuilder();
+		builder.append(id);
+		return builder.hashCode();
+	}
 }
