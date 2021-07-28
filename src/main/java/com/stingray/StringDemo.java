cd1d3faa0d925b6103c6aeb02eb3795aa6750e1e
sandeep.kumar <sandeep.kumar@sa-HP.tsi.local>
2021-07-28 14:47:23 +0530

    average salary

--- src/main/java/com/stingray/StringDemo.java
+++ src/main/java/com/stingray/StringDemo.java
@@ -9,27 +9,25 @@ Strings before swap: a = Hello and b = World
 Strings after swap: a = World and b = Hello
 */
 public class StringDemo {
-	public static void main(String[] args) {}
+	public static void main(String[] args) {
+		withoutUsingThirdVariable();	
+		}
 /**
  * 
  * Swap two Strings without using third user defined variable in Java
  * 
  */
 	
-	void withoutUsingThirdVariable() {
-
+	static void withoutUsingThirdVariable() {
 		String a ="Hello";
 		String b = "World";
 		System.out.println("a ---"+a);
 		System.out.println("b ---"+b);
 
 		a=a+b;
-		b=(String) a.substring(0, a.length()-b.length());
-	
+		b=a.substring(0, a.length()-b.length());	
 		a=a.substring(a.length()-b.length(), a.length());
 		System.out.println("a ---"+a);
-		System.out.println("b ---"+b);
-
-	
+		System.out.println("b ---"+b);	
 	}
 }
